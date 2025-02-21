9b189be566cb4a081f5567d77b4f2a5cb5a881b3
Jamie Bower <j_bower@hotmail.co.uk>
2025-02-21 17:28:13 +1100

    feat: start building drag and drop form menu recipe days

--- src/containers/menus/screens/singleMenu.tsx
+++ src/containers/menus/screens/singleMenu.tsx
@@ -1,4 +1,4 @@
-import { Pressable, ScrollView, StyleSheet, View } from 'react-native'
+import { LayoutChangeEvent, Pressable, ScrollView, StyleSheet, View } from 'react-native'
 import { component, semantic } from '../../../designTokens'
 import { RouteProp, useRoute } from '@react-navigation/native'
 import { MenusStackParamsList } from '../stackNavigator'
@@ -9,10 +9,71 @@ import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIc
 import { MoreOptions } from '../components/moreOptions'
 import { AddRecipe } from '../components/addRecipe'
 import { getDayOptions } from '../components/days/utils'
+import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler'
+
+import { GestureDetector } from 'react-native-gesture-handler'
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated'
+import { ReactNode, useState } from 'react'
+
+type Dropzone = { x: number; y: number; w: number; h: number } | null
+type OnDrop = (finalX: number, finalY: number) => void
+
+function DragableItem({ children, onDrop }: { children: ReactNode; onDrop: OnDrop }) {
+    const isPressed = useSharedValue(false)
+    const offset = useSharedValue({ x: 0, y: 0 })
+
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: offset.value.x }, { translateY: offset.value.y }, { scale: withSpring(isPressed.value ? 1.3 : 1) }]
+        }
+    })
+
+    const start = useSharedValue({ x: 0, y: 0 })
+    const gesture = Gesture.Pan()
+        .onBegin(() => {
+            isPressed.value = true
+        })
+        .onUpdate((e) => {
+            offset.value = {
+                x: e.translationX + start.value.x,
+                y: e.translationY + start.value.y
+            }
+        })
+        .onEnd((e) => {
+            offset.value = {
+                x: 0,
+                y: 0
+            }
+
+            onDrop(e.absoluteX, e.absoluteY)
+        })
+        .onFinalize(() => {
+            isPressed.value = false
+        })
+        .runOnJS(true)
+
+    return (
+        <GestureDetector gesture={gesture}>
+            <Animated.View style={animatedStyles}>{children}</Animated.View>
+        </GestureDetector>
+    )
+}
+
+function useDropzones() {
+    const [dropzones, setDropzones] = useState<Record<string, Dropzone>>({})
+
+    function registerDropzone(id: string, dropzone: Dropzone) {
+        setDropzones((prev) => ({ ...prev, [id]: dropzone }))
+    }
+
+    return { registerDropzone, dropzones }
+}
 
 export function SingleMenuScreen() {
     const route = useRoute<RouteProp<MenusStackParamsList, 'SingleMenu'>>()
     const { menuId } = route.params
+
+    const { dropzones, registerDropzone } = useDropzones()
 
     const { data: singleMenuData, isPending: isSingleMenuPending, isError: isSingleMenuError } = query.menus.single.useQuery(menuId.toString())
     const { data: recipesData, isFetching: isRecipesFetching, isError: isRecipesError } = query.recipes.all.useQuery()
@@ -37,55 +98,80 @@ export function SingleMenuScreen() {
         removeRecipeFromMenu({ menuId: menuId.toString(), recipeId })
     }
 
+    function onDrop(finalX: number, finalY: number) {
+        const targetDropzoneId =
+            Object.entries(dropzones).find(([, dropzone]) => {
+                if (!dropzone) {
+                    return
+                }
+
+                const { x: dzLeft, y: dzTop, w, h } = dropzone
+                const dzRight = dzLeft + w
+                const dzBottom = dzTop + h
+
+                const isInside = finalX > dzLeft && finalX < dzRight && finalY > dzTop && finalY < dzBottom
+
+                return isInside
+            })?.[0] ?? null
+
+        console.log(targetDropzoneId)
+    }
+    console.log(dropzones)
+    function handleRegisterDropzone(e: LayoutChangeEvent, dropzoneId: string) {
+        e.target.measureInWindow((x, y, w, h) => {
+            registerDropzone(dropzoneId, { x, y, w, h })
+        })
+    }
+
     return (
-        <View style={styles.main}>
-            <View style={styles.topContainer}>
-                <View style={styles.titleContainer}>
-                    <Text style={styles.title}>{name}</Text>
-                    <Pressable style={styles.titleEditButton} onPress={() => {}}>
-                        <MaterialCommunityIcon name='square-edit-outline' size={22} color={semantic.colorTextPrimary} />
-                    </Pressable>
+        <GestureHandlerRootView style={styles.main}>
+            <ScrollView>
+                <View style={styles.topContainer}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>{name}</Text>
+                        <Pressable style={styles.titleEditButton} onPress={() => {}}>
+                            <MaterialCommunityIcon name='square-edit-outline' size={22} color={semantic.colorTextPrimary} />
+                        </Pressable>
+                    </View>
+                    <MoreOptions menuId={id} menuName={name} />
                 </View>
-                <MoreOptions menuId={id} menuName={name} />
-            </View>
-            <View style={styles.buttonContainer}>{<AddRecipe menuId={id} />}</View>
-            <Text style={styles.introText}>Drag and drop recipes to change the day.</Text>
-            <ScrollView style={styles.scrollView}>
-                <View style={styles.day}>
-                    <Text style={styles.noDayTitle}>No Day Assigned</Text>
-                    {[...recipes]
-                        .filter(
-                            ({ day_of_week }) =>
-                                !getDayOptions()
-                                    .map((dayOption) => dayOption.date)
-                                    .includes(day_of_week.day || '')
-                        )
-                        .sort((a, b) => (a.name > b.name ? 1 : -1))
-                        .map((recipe) => (
-                            <View style={styles.menuRecipe} key={recipe.id}>
-                                <Text>{recipe.name}</Text>
-                                <View style={styles.actions}>
-                                    <Pressable onPress={() => handleRemoveRecipeFromMenu(recipe.id)}>
-                                        <MaterialCommunityIcon name='delete' size={22} color={semantic.colorTextPrimary} />
-                                    </Pressable>
+                <View style={styles.buttonContainer}>{<AddRecipe menuId={id} />}</View>
+                <Text style={styles.introText}>Drag and drop recipes to change the day.</Text>
+                <View style={styles.mainView}>
+                    <View style={styles.day}>
+                        <Text style={styles.noDayTitle}>No Day Assigned</Text>
+                        {[...recipes]
+                            .filter(
+                                ({ day_of_week }) =>
+                                    !getDayOptions()
+                                        .map((dayOption) => dayOption.date)
+                                        .includes(day_of_week.day || '')
+                            )
+                            .sort((a, b) => (a.name > b.name ? 1 : -1))
+                            .map((recipe) => (
+                                <View style={styles.menuRecipe} key={recipe.id}>
+                                    <DragableItem onDrop={onDrop}>
+                                        <Text>{recipe.name}</Text>
+                                    </DragableItem>
+                                    <View style={styles.actions}>
+                                        <Pressable onPress={() => handleRemoveRecipeFromMenu(recipe.id)}>
+                                            <MaterialCommunityIcon name='delete' size={22} color={semantic.colorTextPrimary} />
+                                        </Pressable>
+                                    </View>
                                 </View>
-                            </View>
-                        ))}
-                </View>
+                            ))}
+                    </View>
 
-                <View style={styles.day}>
-                    <Text style={styles.dayTitle}>Saturday</Text>
-                </View>
+                    <View style={styles.day} onLayout={(e) => handleRegisterDropzone(e, 'sat')}>
+                        <Text style={styles.dayTitle}>Saturday</Text>
+                    </View>
 
-                <View style={styles.day}>
-                    <Text style={styles.dayTitle}>Sunday</Text>
-                </View>
-
-                <View style={styles.day}>
-                    <Text style={styles.dayTitle}>Monday</Text>
+                    <View style={styles.day} onLayout={(e) => handleRegisterDropzone(e, 'sun')}>
+                        <Text style={styles.dayTitle}>Sunday</Text>
+                    </View>
                 </View>
             </ScrollView>
-        </View>
+        </GestureHandlerRootView>
     )
 }
 
@@ -123,7 +209,7 @@ const styles = StyleSheet.create({
     introText: {
         marginTop: 20
     },
-    scrollView: {
+    mainView: {
         marginTop: 25
     },
     menuRecipe: {
